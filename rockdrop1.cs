26fb4f1573ad419793ede52e6ad699c48d1da067
curiosibot <32541369+curiosibot@users.noreply.github.com>
2017-12-10 20:33:56 +0100

    object drop and sound trigger

---
+++ rockdrop1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rockdrop1 : MonoBehaviour 
+{
+	AudioSource audioSource;
+
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		foreach (ContactPoint contact in collision.contacts)
+		{
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+		if (collision.relativeVelocity.magnitude > 1)
+			audioSource.Play();
+	}
+}
